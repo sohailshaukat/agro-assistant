0f71a70ec70daee02fd618c0618ecf138f4fec1c
Sohail Shaukat Selod <sohail47k@gmail.com>
2019-04-24 14:27:40 +0530

    soil genie frame added+backgroun added

--- project.py
+++ project.py
@@ -26,6 +26,22 @@ class GenieCropForm(FlaskForm):
                                                                 ('crop_option_pulses','Pulses')])
     submit = SubmitField('Submit')
 
+class GenieFertilizerForm(FlaskForm):
+
+    soil_type = SelectField(u'Select a Soil type:',choices = [('soil_type_alluvial','Alluvial'),('soil_type_red','Red'),
+                                                            ('soil_type_black','Black'),('soil_type_mountain','Mountain'),
+                                                            ('soil_type_laterite','Laterite'),('soil_type_desert','Desert')])
+
+    crop_option = SelectField(u'Select a Crop kind:', choices =[('crop_option_rice','Rice'),('crop_option_wheat','Wheat'),
+                                                                ('crop_option_sugarcane','Sugarcane'),('crop_option_groundnut','Groundnut'),
+                                                                ('crop_option_apple','Apple'),('crop_option_strawberry','Strawberry'),
+                                                                ('crop_option_maize','Maize'),('crop_option_grapes','Grapes'),('crop_option_coffee','Coffee'),
+                                                                ('crop_option_pulses','Pulses')])
+
+    submit = SubmitField('Submit')
+
+
+
 @app.route('/',methods=['GET','POST'])
 def index():
     return render_template("home.html")
@@ -105,15 +121,21 @@ def desert_soil():
 def soil_genie():
     form_soil = GenieSoilForm()
     form_crop = GenieCropForm()
+    form_fertilizer = GenieFertilizerForm()
     if form_soil.validate_on_submit():
         session['soil_type'] = form_soil.soil_type.data
         return redirect(url_for('soil_genie_result'))
     elif form_crop.validate_on_submit():
         session['crop_option'] = form_crop.crop_option.data
         return redirect(url_for('soil_genie_result'))
+    elif form_fertilizer.validate_on_submit():
+        session['soil_type'] = form_fertilizer.soil_type.data
+        session['crop_option'] = form_fertilizer.crop_option.data
+        return redirect(url_for('soil_genie_result'))
 
 
-    return render_template('soil-genie.html',form_soil = form_soil, form_crop = form_crop)
+
+    return render_template('soil-genie.html',form_soil = form_soil, form_crop = form_crop, form_fertilizer = form_fertilizer)
 
 @app.route('/soil-genie/advice')
 def soil_genie_result():
